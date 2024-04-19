12df6772ae4c7d25a108ed9e0a66df2645e8967a
Sourav Kumar Nanda <souravkumarnanda4@gmail.com>
2024-04-19 23:15:04 +0530

    Delete frames/frames.ts

--- frames/frames.ts
+++
@@ -1,7 +0,0 @@
-import { farcasterHubContext } from "frames.js/middleware";
-import { createFrames, types } from "frames.js/next";
-
-export const frames = createFrames({
-  basePath: "/frames",
-  middleware: [farcasterHubContext()],
-});
